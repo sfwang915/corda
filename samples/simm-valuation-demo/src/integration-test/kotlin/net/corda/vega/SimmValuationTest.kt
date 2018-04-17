ab59e95650759200c1d541b334b08e8073343ca5
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2018-04-17 10:34:00 +0100

    Fix SimmValuationTest

--- samples/simm-valuation-demo/src/integration-test/kotlin/net/corda/vega/SimmValuationTest.kt
+++ samples/simm-valuation-demo/src/integration-test/kotlin/net/corda/vega/SimmValuationTest.kt
@@ -12,6 +12,8 @@ import net.corda.vega.api.PortfolioApi
 import net.corda.vega.api.PortfolioApiUtils
 import net.corda.vega.api.SwapDataModel
 import net.corda.vega.api.SwapDataView
+import net.corda.vega.contracts.IRSState
+import net.corda.vega.flows.IRSTradeFlow
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.math.BigDecimal
@@ -28,7 +30,7 @@ class SimmValuationTest {
 
     @Test
     fun `runs SIMM valuation demo`() {
-        driver(DriverParameters(isDebug = true, extraCordappPackagesToScan = listOf("net.corda.vega.contracts", "net.corda.vega.plugin.customserializers"))) {
+        driver(DriverParameters(isDebug = true, extraCordappPackagesToScan = listOf(IRSTradeFlow.javaClass.`package`.name, IRSState::class.java.`package`.name, "net.corda.vega.plugin.customserializers"))) {
             val nodeAFuture = startNode(providedName = nodeALegalName)
             val nodeBFuture = startNode(providedName = nodeBLegalName)
             val (nodeA, nodeB) = listOf(nodeAFuture, nodeBFuture).map { it.getOrThrow() }
